45e33509c50ace892d574b1eb1b20f2fe7421e20
BabyShung <hao-zheng@uiowa.edu>
2014-01-22 00:02:01 -0600

    leetcode

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,11 @@
 import applications.IntPalidrome;
 
-
 public class Test {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		IntPalidrome ip = new IntPalidrome();
 		System.out.println(ip.isIntPalidrome(234323));
