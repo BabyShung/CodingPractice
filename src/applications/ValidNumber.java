d37ee4e673e3ce4d59754719a6b79a084fc15ede
BabyShung <hao-zheng@uiowa.edu>
2014-03-06 19:30:44 -0600

    updated two valids

--- src/applications/ValidNumber.java
+++ src/applications/ValidNumber.java
@@ -1,5 +1,15 @@
 package applications;
 
+/**
+ * Validate if a given string is numeric.
+ * 
+ * Some examples: "0" => true " 0.1 " => true "abc" => false "1 a" => false
+ * "2e10" => true
+ * 
+ * @author haozheng
+ * 
+ */
+
 public class ValidNumber {
 
 	// reg exp:
@@ -18,5 +28,55 @@ public class ValidNumber {
 		}
 		return s.charAt(s.length() - 1) != 'f'
 				&& s.charAt(s.length() - 1) != 'd';
+	}
+
+	/**
+	 * Using FSM (finite state machine) O(n) time and O(5*9) = O(1) space
+	 * 
+	 */
+	private enum Type {
+		Space(0), Sign(1), Digit(2), Dot(3), Exp(4), Null(-1);
+		private int type;
+
+		private Type(int t) {
+			type = t;
+		}
+
+		public int getType() {
+			return type;
+		}
+	}
+
+	public boolean isNumber3(String s) {
+		int[][] states = { { 0, 8, -1, -1, 8, -1, -1, 8, 8 },
+				{ 2, -1, -1, -1, -1, 6, -1, -1, -1 },
+				{ 1, 1, 1, 4, 4, 7, 7, 7, -1 },
+				{ 3, 4, 3, -1, -1, -1, -1, -1, -1 },
+				{ -1, 5, -1, -1, 5, -1, -1, -1, -1 } };
+
+		int state = 0;
+		for (Character c : s.toCharArray()) {
+			Type inputType = Type.Null;
+			if (c == ' ') {
+				inputType = Type.Space;
+			} else if (c == '+' || c == '-') {
+				inputType = Type.Sign;
+			} else if (c >= '0' && c <= '9') {
+				inputType = Type.Digit;
+			} else if (c == '.') {
+				inputType = Type.Dot;
+			} else if (c == 'e' || c == 'E') {
+				inputType = Type.Exp;
+			} else {
+				return false; // invalid input
+			}
+
+			state = states[inputType.getType()][state];
+			if (state < 0) { // invalid state transition
+				return false;
+			}
+		}
+
+		return (state == 1 || state == 4 || state == 7 || state == 8);
 	}
 }
--- src/applications/ValidPalindrome.java
+++ src/applications/ValidPalindrome.java
@@ -1,5 +1,16 @@
 package applications;
 
+/**
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ * 
+ * For example, "A man, a plan, a canal: Panama" is a palindrome. "race a car"
+ * is not a palindrome.
+ * 
+ * @author haozheng
+ * 
+ */
+
 public class ValidPalindrome {
 	// public boolean isPalindrome(String s) {
 	// if(s.length()==0)
@@ -8,37 +19,38 @@ public class ValidPalindrome {
 	// s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 	// return checkPalindrome(s);
 	// }
-    
-    public boolean isPalindrome(String s) {
-        if(s.length()==0)
-            return true;
-        
-        s= normalize(s);  
-        return checkPalindrome(s);
-    }
-    
-    private String normalize(String s){
-        
-        StringBuilder sb = new StringBuilder();
-        char tmp;
-        for(int i = 0; i<s.length();i++){
-            tmp = s.charAt(i);
-            if((tmp<'a'||tmp>'z')&&(tmp<'A'||tmp>'Z')&&(tmp<'0'||tmp>'9')){
-                continue;
-            }
-            if((tmp<='Z'&&tmp>='A'))//lower case
-                tmp = (char)(tmp-'A'+'a');
-            sb.append(tmp);
-        }
-        return sb.toString();
-    }
-    
-    private boolean checkPalindrome(String s){
-        int len = s.length();
-        for(int i = 0;i<len/2;i++){
-            if(s.charAt(i)!=s.charAt(len-1-i))
-                return false;
-        }
-        return true;
-    }
+
+	public boolean isPalindrome(String s) {
+		if (s.length() == 0)
+			return true;
+
+		s = normalize(s);
+		return checkPalindrome(s);
+	}
+
+	private String normalize(String s) {
+
+		StringBuilder sb = new StringBuilder();
+		char tmp;
+		for (int i = 0; i < s.length(); i++) {
+			tmp = s.charAt(i);
+			if ((tmp < 'a' || tmp > 'z') && (tmp < 'A' || tmp > 'Z')
+					&& (tmp < '0' || tmp > '9')) {
+				continue;
+			}
+			if ((tmp <= 'Z' && tmp >= 'A'))// lower case
+				tmp = (char) (tmp - 'A' + 'a');
+			sb.append(tmp);
+		}
+		return sb.toString();
+	}
+
+	private boolean checkPalindrome(String s) {
+		int len = s.length();
+		for (int i = 0; i < len / 2; i++) {
+			if (s.charAt(i) != s.charAt(len - 1 - i))
+				return false;
+		}
+		return true;
+	}
 }
