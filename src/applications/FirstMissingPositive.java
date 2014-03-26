e2d90638ab5e3c681c44fce766579ffa069f8c44
BabyShung <hao-zheng@uiowa.edu>
2014-03-25 21:14:14 -0500

    added intro

--- src/applications/FirstMissingPositive.java
+++ src/applications/FirstMissingPositive.java
@@ -1,17 +1,50 @@
 package applications;
 
+/**
+ * firstMissingPositive
+ * 
+ * Given an unsorted integer array, find the first missing positive integer. For
+ * example, given [1,2,0] return 3, and [3,4,-1,1] return 2. Your algorithm
+ * should run in O(n) time and uses constant space.
+ * 
+ * difficult:
+ * 
+ * The idea is that in an unsorted array, get the first missing positive
+ * integer. EG: { -8,1,2,3,4,5,7,8,9,-1,-2}
+ * 
+ * Since the positive integer starts from 1, the FMP in this case is 6. How to
+ * do that in O(n) time?
+ * 
+ * If you have a positive int at index i (A[i] = x), we should swap it to A[x-1]
+ * as the right place
+ * 
+ * EG: if A[5] = 1, then 1 should be swapped to A[1-1] = A[0] ------> A[0] = 1
+ * 
+ * 
+ * So the first scan, should have four conditions
+ * 
+ * 1.if negative, ignore
+ * 
+ * 2.if positive, check if A[i] <= A.length
+ * 
+ * 3.if A[i] != i + 1
+ * 
+ * 4.if A[i] != A[A[i] - 1]
+ * 
+ * if all four is satisfied, swap A[i] and A[A[i] - 1]. Otherwise, i++
+ * 
+ * 
+ * @author haozheng
+ * 
+ */
+
 public class FirstMissingPositive {
 	public int firstMissingPositive(int[] A) {
 		// in-place swap (bucket sort)
 		int i = 0;
-		for(int tmp: A){
-			System.out.print(tmp+" ");
-		}
-		System.out.println();
 		while (i < A.length) {
 			if (A[i] > 0 && A[i] <= A.length && A[i] != i + 1
 					&& A[i] != A[A[i] - 1]) {
-				System.out.println("Swapping: "+A[i]+" and "+A[A[i] - 1]);
 				int temp = A[A[i] - 1];
 				A[A[i] - 1] = A[i];
 				A[i] = temp;
@@ -21,11 +54,7 @@ public class FirstMissingPositive {
 		}
 		// find the first positive missing integer
 		i = 0;
-		
-		for(int tmp: A){
-			System.out.print(tmp+" ");
-		}
-		System.out.println();
+
 		while (i < A.length && A[i] == i + 1)
 			++i;
 		return i + 1;
@@ -36,7 +65,7 @@ public class FirstMissingPositive {
 		for (int i = 0; i < A.length; i++) {
 			while (A[i] > 0 && A[i] <= A.length && A[i] != i + 1
 					&& A[i] != A[A[i] - 1]) {
-				int temp = A[A[i] - 1];//swap
+				int temp = A[A[i] - 1];// swap
 				A[A[i] - 1] = A[i];
 				A[i] = temp;
 			}
