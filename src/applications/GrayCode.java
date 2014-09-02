21bdbd9ee0c13c97a991be3bd428eea049f8b183
BabyShung <hao-zheng@uiowa.edu>
2014-09-02 10:55:24 -0500

    added kthGrayCode

--- src/applications/GrayCode.java
+++ src/applications/GrayCode.java
@@ -1,6 +1,7 @@
 package applications;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The gray code is a binary numeral system where two successive values differ
@@ -18,24 +19,25 @@ import java.util.ArrayList;
 
 public class GrayCode {
 
-	public ArrayList<Integer> grayCode(int n) {
-		// remember: 1<<n means 2^n -> 1<<0 = 2^0 = 1; 1<<1 = 2^1 = 2 ...
-		// the size is generalized from 0,1; 00,01,11,10;
-		// 000,001,011,010,110,111,101,100;
+	// remember: 1<<n means 2^n -> 1<<0 = 2^0 = 1; 1<<1 = 2^1 = 2 ...
+	// the size is generalized from 0,1; 00,01,11,10;
+	// 000,001,011,010,110,111,101,100;
+	public List<Integer> grayCode(int n) {
 
-		ArrayList<Integer> results = new ArrayList<Integer>(1 << n);
-		results.add(0);
-		System.out.println(Integer.toBinaryString(0) + " --- must have");
+		ArrayList<Integer> r = new ArrayList<>(1 << n);
+
+		r.add(0);
+
 		for (int i = 0; i < n; i++) {
 			int flipper = 1 << i;
-			for (int j = results.size() - 1; j >= 0; --j) {
 
-				int newBit = results.get(j) | flipper;
-				results.add(newBit);
-				System.out.println(Integer.toBinaryString(newBit));
+			for (int j = r.size() - 1; j >= 0; j--) {
+				r.add(flipper | r.get(j));
 			}
 		}
-		return results;
+
+		return r;
+
 	}
 
 }
---
+++ src/applications/KthGrayCode.java
@@ -0,0 +1,31 @@
+package applications;
+
+/**
+ * Given a non-negative integer n, representing the total number of bits in the
+ * gray code, and a non-negative integer k, print the k-th code in the gray code
+ * sequence. A gray code sequence must begin with 0.
+ * 
+ * Why we interest in k-th code?
+ * 
+ * Notice that Gray Code can be used to solve Tower of Hanoi problem in that the
+ * bit sequence we flips in each step is same as the ordinal of the disk to be
+ * moved in each step. E.g. suppose n=3, to generate a gray code sequence, we
+ * flips bits of previous number in the following order: 1 - 2 - 1 - 3 - 1 - 2 -
+ * 1
+ * 
+ * code  ordial
+ *  000     000   <- same
+ *	001     001   <- same
+ *	011     010   <- flip bit 0
+ *	010     011   <- flip bit 0
+ *	110     100   <- flip bit 1
+ *	111     101   <- flip bit 1
+ *	101     110   <- flip bits 11
+ * 	100     111   <- flip bits 11
+ * 
+ * @author haozheng
+ *
+ */
+public class KthGrayCode {
+
+}
